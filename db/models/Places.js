c0deec68ca05b9d7f93155f5adf2c22a80e7fce8
Jonas Laß <johnny.jones@gmx.net>
2023-06-21 14:23:24 +0200

    fix connection bug

--- db/models/Places.js
+++ db/models/Places.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const placesSchema = new Schema({
+const placeSchema = new Schema({
   place: { type: String, required: true },
   id: { type: String, required: true },
 });
 
-const Place = mongoose.models.Place || model("Place", placesSchema);
+const Place = mongoose.models.Place || model("Place", placeSchema);
 
 export default Place;
